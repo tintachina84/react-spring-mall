00b85feb3c33be2b34a77ed08ed38cd5db05e2ce
seonghyeon.seo <tintachina84@gmail.com>
2024-04-23 20:26:37 +0900

    Added: layouts.

---
+++ frontend/mall/src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import BasicLayout from "../layouts/BasicLayout"
+
+const AboutPage = () => {
+    return (
+        <BasicLayout>
+            <div className="text-3xl">
+                <h1>About Page</h1>
+            </div>
+        </BasicLayout>
+    )
+}
+
+export default AboutPage
